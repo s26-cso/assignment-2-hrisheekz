a0981d3ba28c7495fb9415cc10f1e0d5648de3f1
Hrisheek Patnala <hrisheek.epic@gmail.com>
2026-04-14 17:58:14 +0530

    Update q4.c

---
+++ q4/q4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h> 
+
+typedef int (*fptr)(int,int);
+
+int main(){
+    char op[10];
+    int num1, num2;
+
+    while(1){
+        scanf("%s %d %d", op, &num1, &num2);
+        char lib_name[32];
+        snprintf(lib_name,sizeof(lib_name),"./lib%s.so",op);
+
+        void* handle=dlopen(lib_name, RTLD_LAZY);
+        if(!handle) continue;
+
+
+        fptr operation=(fptr)dlsym(handle, op);
+        if(!operation){
+            dlclose(handle);
+            continue;
+        }
+
+        int result=operation(num1,num2);
+        printf("%d\n",result);
+        dlclose(handle);
+    }
+
+    return 0;
+}
